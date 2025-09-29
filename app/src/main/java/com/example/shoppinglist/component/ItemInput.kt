fd3ab3a78b2489520859248ca1e5f361978ecba4
FarhanArva <farhanarvaamanta@gmail.com>
2025-09-29 20:30:29 +0700

    GANTI WARNA TIPIS

--- app/src/main/java/com/example/shoppinglist/component/ItemInput.kt
+++ app/src/main/java/com/example/shoppinglist/component/ItemInput.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
 
+
 @Composable
 fun ItemInput(text: String, onTextChange: (String) -> Unit, onAddItem: () -> Unit) {
     Row(
